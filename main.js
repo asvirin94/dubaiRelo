271b9196a0236e89794cd662010888d01e9fabcb
a.svirin <a.svirin94@gmail.com>
2023-04-22 20:07:09 +0400

    effdad

--- main.js
+++ main.js
@@ -3,14 +3,14 @@ const tgIcon = document.querySelector('.tg-icon');
 const waIcon = document.querySelector('.wa-icon');
 
 buttons.forEach((button) => button.addEventListener('click', () => {
-    window.open('https://t.me/durov', '_blank')
+    window.open('https://t.me/iniands', '_blank')
     })
 )
 
 tgIcon.addEventListener('click', () => {
-    window.open('https://t.me/durov', '_blank')
+    window.open('https://t.me/iniands', '_blank')
 })
 
 waIcon.addEventListener('click', () => {
-    window.open('https://t.me/durov', '_blank')
+    window.open('https://wa.me/79264640723', '_blank')
 })
